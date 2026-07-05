3bb48e048272be104b70dd63f2c19f97a70412b6
Raja Shravan <raja@revv.xyz>
2026-07-05 13:48:02 -0400

    small re-write of bbq 2

--- bounded_blocking_queue_2.py
+++ bounded_blocking_queue_2.py
@@ -8,24 +8,39 @@ class BoundedBlockingQueue:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.queue = []
+
+        # we need a lock to manage the queu
         self.lock = threading.Lock()
-        self.not_full = threading.Condition(self.lock)
-        self.not_empty = threading.Condition(self.lock)
+
+        # we use 2 condition variables
+        # naming convention:
+        # name the variable after the condition that you WANT to be true
+        # in order for waiting threads to be woken up.
+        self.has_space = threading.Condition(self.lock) # for put calls. we name it has_space because put calls need space to be avail for them to proceed
+        self.has_items = threading.Condition(self.lock)
 
     def put(self, item):
-        with self.not_full:
+        with self.has_space:
             while len(self.queue) == self.capacity:
-                self.not_full.wait()      # release lock and go to sleep atomically
+                # queue is full, so we need to wait and sleep in one atomic action
+                self.has_space.wait() # wait for this to no longer be true
+            
+            # if queue is not full, we can place on to it, and tell people stuff is there
             self.queue.append(item)
-            self.not_empty.notify()       # wake up a waiting get()
+            self.has_items.notify()
 
     def get(self):
-        with self.not_empty:
+        # in order to get/read from the queue, we use the has_items cond variable
+        with self.has_items:
             while len(self.queue) == 0:
-                self.not_empty.wait()     # release lock and go to sleep atomically
-            item = self.queue.pop(0)
-            self.not_full.notify()        # wake up a waiting put()
-            return item
+                # this condition is not true, so we release and wait on the lock
+                self.has_items.wait()
+            
+            # if the above IF never triggered, then we know that
+            # we have elements in the queue to read from
+            item_to_return = self.queue.pop(0) # get the element
+            self.has_space.notify() # the queue has space now, tell threads waiting on it
+            return item_to_return
 
 
 # --- Test harness ---
